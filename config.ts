5daa590de4c17073f59736ff7c12063304d812ba
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-05-07 19:38:19 +0100

    url5

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export const HTTP = () => {
   let url;
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     //   development
-    return (url = 'https://statuesque-entremet-93144a.netlify.app');
+    return (url = 'http://localhost:3000');
   } else {
     // production code
     return (url = "https://statuesque-entremet-93144a.netlify.app");
