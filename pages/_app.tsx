d2c14c0ba2e199e40561d040106b644d10a456be
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-05-22 19:19:11 +0100

    analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,30 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import Layout from '../components/Layout';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Script
+        // google analitics1.1.
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script
+        strategy='lazyOnload'
+        // google analitics1.2.
+      >
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+`}
+      </Script>
+
       <Layout title='best to do app plus 7 days calendar'>
         <Component {...pageProps} />
       </Layout>
