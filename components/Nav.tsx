171451643c08322592a4f3b56d4183f8b5ccd2c9
Marius Zebrauskas <zebrauskas.mar@gmail.com>
2022-05-30 10:12:40 +0100

    mobile faq2

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -261,8 +261,8 @@ const Nav = () => {
                     onClick={() => changePageMenuBackground('faq')}
                     className={
                       page === 'faq'
-                        ? 'bg-gray-900 text-white  px-3 py-2 rounded-md text-sm font-medium'
-                        : ' text-gray-300 px-3 py-2 rounded-md text-sm font-medium'
+                        ? 'text-white block px-3 py-2 rounded-md text-base font-medium'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
                     }
                   >
                     F.A.Q.
